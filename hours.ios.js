9426aecdb04a9af9c361dfd58fe002c82a4be03c
Mark Pemburn <mark@pemburn.com>
2016-03-10 21:26:57 -0500

    Add port of SunCalc.js and implement rudimentary sunrise/sunset calculation.

--- hours.ios.js
+++ hours.ios.js
@@ -1,4 +1,5 @@
 'use strict';
+var suncalc = require("./suncalc.js");
 
 var React = require('react-native');
 var {
@@ -7,6 +8,10 @@ var {
   Text,
   Component
 } = React;
+// Today in Baltimore, MD
+var date = new Date();
+var lat = 39.2;
+var lng = -76.6;
 
 var styles = StyleSheet.create({
   description: {
@@ -24,13 +29,23 @@ var styles = StyleSheet.create({
 
 class Hours extends Component {
   render() {
+    var result = suncalc.calc(date, lat, lng);
+    var sunrise = this.toJSONLocal(result.sunrise);
+    var sunset = this.toJSONLocal(result.sunset);
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
-          This is the Hours tab.
+          This is the Hours tab.{'\n'}
+          Sunrise: {sunrise}{'\n'}
+          Sunset: {sunset}
         </Text>
       </View>
     );
+  }
+  toJSONLocal (date) {
+  	var local = new Date(date);
+  	local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  	return local.toJSON().slice(11, -1);
   }
 }
 
